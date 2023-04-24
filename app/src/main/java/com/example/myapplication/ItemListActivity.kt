d1b45c9dfe129a7f9d33f47046c208a37944dd4b
danhavranek <daniel7861@mailbox.org>
2023-04-24 14:26:09 +0200

    Update item list activity

--- app/src/main/java/com/example/myapplication/ItemListActivity.kt
+++ app/src/main/java/com/example/myapplication/ItemListActivity.kt
@@ -9,6 +9,7 @@ import com.example.myapplication.databinding.ActivityItemListBinding
 class ItemListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityItemListBinding
+    private lateinit var itemCategoryAdapter: ItemCategoryAdapter
     private lateinit var itemAdapter: ItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +17,19 @@ class ItemListActivity : AppCompatActivity() {
         binding = ActivityItemListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var exampleItems = mutableListOf<Item>()
-        exampleItems.add(Item("Example 1", "Category 1", Picture(), 0, 5))
-        exampleItems.add(Item("Example 2", "Category 1", Picture(), 3, 4))
-        exampleItems.add(Item("Example 3", "Category 2", Picture(), 1, 1))
+        var exampleItems1 = mutableListOf<Item>()
+        exampleItems1.add(Item("Example 1", "Category 1", Picture(), 0, 5))
+        exampleItems1.add(Item("Example 2", "Category 1", Picture(), 3, 4))
+        var exampleItems2 = mutableListOf<Item>()
+        exampleItems2.add(Item("Example 3", "Category 2", Picture(), 1, 1))
 
-        itemAdapter = ItemAdapter(exampleItems)
-        binding.rvItems.adapter = itemAdapter
-        binding.rvItems.layoutManager = LinearLayoutManager(this)
+        var exampleCategories = mutableListOf<ItemCategory>()
+        exampleCategories.add(ItemCategory("Category 1", exampleItems1))
+        exampleCategories.add(ItemCategory("Category 2", exampleItems2))
+
+        itemCategoryAdapter = ItemCategoryAdapter(exampleCategories)
+        binding.rvCategories.adapter = itemCategoryAdapter
+        binding.rvCategories.layoutManager = LinearLayoutManager(this)
 
 
     }
