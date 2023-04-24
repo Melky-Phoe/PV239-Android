dbefd1e60e100503244f50e18276e4a3949c606f
melkus <josefmelkus@seznam.cz>
2023-04-24 10:26:05 +0200

    data class

--- app/src/main/java/com/example/myapplication/Item.kt
+++ app/src/main/java/com/example/myapplication/Item.kt
@@ -1,5 +1,10 @@
 package com.example.myapplication
 
+import android.graphics.Picture
+
 data class Item(
-    val name: String
+    val name: String,
+    val picture: Picture,
+    val currentCount: Int,
+    val totalCount: Int,
 )
