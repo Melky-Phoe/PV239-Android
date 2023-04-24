a924b185c641174bbccd21c499c7b5a689bfc5d6
melkus <josefmelkus@seznam.cz>
2023-04-24 18:09:57 +0200

    add item

--- app/src/main/java/com/example/myapplication/ItemFragment.kt
+++ app/src/main/java/com/example/myapplication/ItemFragment.kt
@@ -1,42 +1,52 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.ItemBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ItemFragment : Fragment() {
 
-    private var _binding: ItemBinding? = null
+    private lateinit var binding: ItemBinding
 
-    private var _totalCount = 1
-    private var _currentCount = 0
+    private var _totalCount = 3
+    private var _currentCount: Int = 0
+        set(value) {
+            field = value
+            onCurrentCountUpdate()
+        }
+
+    //private var _currentCount = MutableLiveData<Int>()
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    //private val binding get() = _binding
 
+//    private val adapter: ItemAdapter by lazy {
+//        ItemAdapter()
+//    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = ItemBinding.inflate(inflater, container, false)
+    ): View {
+        binding = ItemBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.itemCounterText.text = "$_currentCount/$_totalCount"
         binding.editButton.setOnClickListener {
-            // TODO navigate to edit
-            // findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_itemFragment_to_itemAddEditFragment)
         }
 
         binding.plusButton.setOnClickListener {
@@ -63,8 +73,11 @@ class ItemFragment : Fragment() {
         }
     }
 
+    private fun onCurrentCountUpdate() {
+        binding.itemCounterText.text = "$_currentCount/$_totalCount"
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 }
