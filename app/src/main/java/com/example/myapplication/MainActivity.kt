7f2d18c5b7b73d7236c6f07ee580fe57d37d4d5c
danhavranek <daniel7861@mailbox.org>
2023-05-01 08:24:46 +0200

    Clean up main activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,39 +1,17 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.fragment.NavHostFragment
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        // TODO dat seznam seznamu a pak hledat spravny seznam podle ID
-        //val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        //val navController = navHostFragment.navController
-        // binding.ListOfLists.setupWithNavController(navController)
-
-        // Prevzato z Consumption, nevim co to presne znamena
-//        navController.addOnDestinationChangedListener { _, destination, _ ->
-//            binding.bottomNavigation.isVisible = destination.id != R.id.consumptionAddEditFragment
-//        }
+        
     }
 }
