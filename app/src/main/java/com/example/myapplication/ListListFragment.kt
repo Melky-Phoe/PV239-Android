60cf5dcfaaace1b8886f1bb54f568e4ce129a6c4
michalsum4 <michalsum4@gmail.com>
2023-05-02 22:43:07 +0200

    added addPackerList button - working but no with database(not implemented yet)

--- app/src/main/java/com/example/myapplication/ListListFragment.kt
+++ app/src/main/java/com/example/myapplication/ListListFragment.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.FragmentListListBinding
@@ -30,13 +32,17 @@ class ListListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentListListBinding.inflate(layoutInflater, container, false)
-        adapter = PackerListAdapter() {
+        adapter = PackerListAdapter {
             showListItems(it)
         }
         binding.rvItems.layoutManager = LinearLayoutManager(requireContext())
         binding.rvItems.adapter = adapter
+
+        binding.addListButton.setOnClickListener {
+            showAddPackerListDialog()
+        }
 
         val listsObserver = Observer<List<PackerList>> { items ->
             // Update list adapter
@@ -48,8 +54,43 @@ class ListListFragment : Fragment() {
         return binding.root
     }
 
-    fun showListItems(packerList: PackerList) {
+    private fun showListItems(packerList: PackerList) {
         appNavigator.navigateToItemList(packerList)
+    }
+
+    private fun showAddPackerListDialog() {
+        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.new_packer_list_dialog, null)
+        val listNameEditText = dialogView.findViewById<EditText>(R.id.et_list_name)
+
+        AlertDialog.Builder(requireActivity())
+            .setTitle("Add New Packer List")
+            .setView(dialogView)
+            .setPositiveButton("Create") { _, _ ->
+                val listName = listNameEditText.text.toString().trim()
+                if (listName.isNotEmpty()) {
+                    createNewPackerList(listName)
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
+    }
+
+    private fun createNewPackerList(listName: String) {
+        val newPackerList = PackerList(name = listName, items = emptyList())
+
+        // Add the new PackerList to your Firebase Realtime Database
+        // You may need to modify this code to match your existing database structure
+        //val database = FirebaseDatabase.getInstance()
+        //val packerListReference = database.getReference("packerLists").push()
+
+        //packerListReference.setValue(newPackerList)
+
+        // temporary solution - for testing
+        val newList = adapter.currentList.toMutableList()
+        newList.add(newPackerList)
+
+        adapter.submitList(newList)
     }
 
 }
