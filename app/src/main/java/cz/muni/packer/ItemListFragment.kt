9bdc72ff7a23a1292452632405342687245b0747
danhavranek <daniel7861@mailbox.org>
2023-05-05 22:16:35 +0200

    Update ItemListFragment to use category adapter

--- app/src/main/java/cz/muni/packer/ItemListFragment.kt
+++ app/src/main/java/cz/muni/packer/ItemListFragment.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import cz.muni.packer.data.Category
 import cz.muni.packer.data.Item
 import cz.muni.packer.databinding.FragmentItemListBinding
 import cz.muni.packer.repository.ItemRepository
@@ -21,7 +22,7 @@ import cz.muni.packer.repository.ItemRepository
 class ItemListFragment : Fragment() {
 
     private lateinit var binding: FragmentItemListBinding
-    private lateinit var adapter: ItemAdapter
+    private lateinit var adapter: CategoryAdapter
     private lateinit var appNavigator: AppNavigator
     private val args: ItemListFragmentArgs by navArgs()
 
@@ -40,17 +41,15 @@ class ItemListFragment : Fragment() {
     ): View {
         binding = FragmentItemListBinding.inflate(layoutInflater, container, false)
 
-        adapter = ItemAdapter(
-            onItemClick = { item ->
-                showItemDetails(item)
-            },
-            onCountUpdate = { item ->
-                updateItemCount(item)
-            }
-        )
+        val packerListItems = itemRepository.getItemsForPackerList(args.packerListId)
+        val itemMap = packerListItems.groupBy { it.category }
+        val categoryItemList = itemMap.map { (category, itemList) ->
+            Category(category.name, itemList)
+        }
+
+        adapter = CategoryAdapter(categoryItemList, appNavigator, itemRepository)
         binding.rvItems.layoutManager = LinearLayoutManager(requireContext())
         binding.rvItems.adapter = adapter
-        adapter.submitList(itemRepository.getItemsForPackerList(args.packerListId))
 
         binding.fab.setOnClickListener {
             val action = ItemListFragmentDirections.actionItemListFragmentToItemAddEditFragment(packerListId = args.packerListId)
@@ -60,23 +59,6 @@ class ItemListFragment : Fragment() {
         // activity?.findViewById<Toolbar>(R.id.toolbar)?.title = args.packerListId.toString()
 
         return binding.root
-    }
-
-    private fun showItemDetails(item: Item) {
-        appNavigator.navigateToItemDetails(item)
-    }
-
-    private fun updateItemCount(item: Item) {
-//        itemRepository.updateItem(item)
-        itemRepository.updateCount(item.id, item.currentCount)
-    }
-
-    private fun refreshList() {
-        adapter.submitList(itemRepository.getItemsForPackerList(args.packerListId))
-    }
-    override fun onResume() {
-        super.onResume()
-        refreshList()
     }
 
 }
